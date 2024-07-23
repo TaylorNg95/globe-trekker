add5f285a8ae9719bf510a4508ae72bdfb6e99cd
Taylor Ng <taylormng95@gmail.com>
2024-07-23 14:07:18 -0400

    configures metaData for model naming conventions

--- server/config.py
+++ server/config.py
@@ -1,10 +1,21 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from sqlalchemy.schema import MetaData
+
+naming_convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
+metaData = MetaData(naming_convention=naming_convention)
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db' # TBD: link to .env file
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app=app)
+db = SQLAlchemy(app=app, metadata=metaData)
 migrate = Migrate(app=app, db=db, render_as_batch=True) # TBD: added this in
