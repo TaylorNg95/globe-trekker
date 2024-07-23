dc10eb8a2d3ef1cd38ddf7885d242775d16e6b0b
Taylor Ng <taylormng95@gmail.com>
2024-07-23 16:22:41 -0400

    creates entries model

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from config import app
 from models.user import User
 from models.trip import Trip
+from models.entry import Entry
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
---
+++ server/models/entry.py
@@ -0,0 +1,30 @@
+from config import db
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.orm import validates
+import re
+
+class Entry(db.Model, SerializerMixin):
+    __tablename__ = 'entries'
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String, nullable=False)
+    miles = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    trip_id = db.Column(db.Integer, db.ForeignKey('trips.id'), nullable=False)
+
+    def __repr__(self):
+        return f'<Entry date={self.date}, miles={self.miles}, user_id={self.user_id}, trip_id={self.trip_id}>'
+
+    @validates('date')
+    def validate_date(self, key, date):
+        if not re.fullmatch(r'\d{2}-\d{2}-\d{2}', date):
+            raise ValueError('Invalid date format')
+        else:
+            return date
+        
+    @validates('miles')
+    def validate_miles(self, key, miles):
+        if miles <= 0:
+            raise ValueError('Miles must be greater than 0')
+        else:
+            return miles
