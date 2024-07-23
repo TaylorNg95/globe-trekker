da53bd83e09152ad9b3b6fb1d74018d5e5a76e2d
Taylor Ng <taylormng95@gmail.com>
2024-07-23 14:02:17 -0400

    configures SQLAlchemy and Migrate; inits database

--- server/config.py
+++ server/config.py
@@ -1,3 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db' # TBD: link to .env file
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app=app)
+migrate = Migrate(app=app, db=db, render_as_batch=True) # TBD: added this in
---
+++ server/migrations/versions/48b033dc0f45_init_database.py
@@ -0,0 +1,24 @@
+"""init database
+
+Revision ID: 48b033dc0f45
+Revises: 
+Create Date: 2024-07-23 14:01:12.652004
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '48b033dc0f45'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
