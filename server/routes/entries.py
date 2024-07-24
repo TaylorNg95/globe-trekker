ce954a92e6a56455bd8ce02333155a7b5fc73429
Taylor Ng <taylormng95@gmail.com>
2024-07-24 17:28:27 -0400

    adds delete function to Entry Resource

--- server/routes/entries.py
+++ server/routes/entries.py
@@ -24,12 +24,12 @@ class UserEntriesResource(Resource):
             return {'error': 'Invalid input'}, 422
     
 class UserEntryResource(Resource):
-     def get(self, user_id, trip_id, entry_id):
+    def get(self, user_id, trip_id, entry_id):
         entries = Entry.query.filter(Entry.user_id == user_id, Entry.trip_id == trip_id)
         entry = [entry for entry in entries if entry.id == entry_id][0]
         return entry.to_dict(rules=['-user', '-trip']), 200
      
-     def patch(self, user_id, trip_id, entry_id):
+    def patch(self, user_id, trip_id, entry_id):
          data = request.get_json()
          date = data.get('date')
          miles = data.get('miles')
@@ -43,6 +43,14 @@ class UserEntryResource(Resource):
          except:
              {'error': 'Invalid input'}, 422
 
+    def delete(self, user_id, trip_id, entry_id):
+        entry = Entry.query.filter(Entry.id == entry_id).first()
+        try:
+            db.session.delete(entry)
+            db.session.commit()
+            return {}, 204
+        except:
+            return {'error': 'Invalid user id'}
 
 api.add_resource(UserEntriesResource, '/api/users/<int:user_id>/trips/<int:trip_id>/entries')
 api.add_resource(UserEntryResource, '/api/users/<int:user_id>/trips/<int:trip_id>/entries/<int:entry_id>')
