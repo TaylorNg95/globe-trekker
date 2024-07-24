2c94cb89e0f765239bb9343e692c8503030c1d8e
Taylor Ng <taylormng95@gmail.com>
2024-07-24 19:18:11 -0400

    sets up main structure of pages

---
+++ client/src/components/navigation/NavBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function NavBar() {
+    return (
+        <h1>In Nav Bar</h1>
+    )
+}
+
+export default NavBar
---
+++ client/src/components/trips/TripMenu.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TripMenu() {
+  return (
+    <div>In Trip Menu</div>
+  )
+}
+
+export default TripMenu
