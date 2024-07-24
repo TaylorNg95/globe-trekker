a39073b7dab89d7e04e7f4d691e017ea4cc39f26
Taylor Ng <taylormng95@gmail.com>
2024-07-24 16:25:55 -0400

    corrects error in filtering with multiple conditions for entry resource

--- server/routes/entries.py
+++ server/routes/entries.py
@@ -6,14 +6,14 @@ from models.entry import Entry
 
 class UserEntriesResource(Resource):
     def get(self, user_id, trip_id):
-        entries = Entry.query.filter(Entry.user_id == user_id and Entry.trip_id == trip_id)
+        entries = Entry.query.filter(Entry.user_id == user_id, Entry.trip_id == trip_id)
         return [entry.to_dict(rules=['-trip', '-user',]) for entry in entries]
     
 class UserEntryResource(Resource):
      def get(self, user_id, trip_id, entry_id):
-        entries = Entry.query.filter(Entry.user_id == user_id and Entry.trip_id == trip_id)
+        entries = Entry.query.filter(Entry.user_id == user_id, Entry.trip_id == trip_id)
         entry = [entry for entry in entries if entry.id == entry_id][0]
-        return entry.to_dict(rules=['-user']), 200
+        return entry.to_dict(rules=['-user', '-trip']), 200
 
 api.add_resource(UserEntriesResource, '/api/users/<int:user_id>/trips/<int:trip_id>/entries')
 api.add_resource(UserEntryResource, '/api/users/<int:user_id>/trips/<int:trip_id>/entries/<int:entry_id>')
