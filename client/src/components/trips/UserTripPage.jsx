48660724f7ce5842a00f90bf8ede76a3ff7c3f1e
Taylor Ng <taylormng95@gmail.com>
2024-07-25 18:05:22 -0400

    completes UserTripPage

--- client/src/components/trips/UserTripPage.jsx
+++ client/src/components/trips/UserTripPage.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import { useContext } from 'react'
+import { useParams } from 'react-router-dom'
+import { TripContext } from '../../context/TripContext'
+import { UserContext } from '../../context/UserContext'
+import EntryList from './entries/EntryList'
 
 function UserTripPage() {
+  const {trips} = useContext(TripContext)
+  const {user} = useContext(UserContext)
+  const {id} = useParams()
+  const trip = trips.find(trip => trip.id == id)
+  const entries = user.entries.filter(entry => entry.trip_id == id)
+
   return (
-    <div>UserTripPage</div>
+    <>
+      <h1>{trip.name}</h1>
+      <h2>{trip.total_miles}</h2>
+      <EntryList entries={entries}/>
+    </>
   )
 }
 
