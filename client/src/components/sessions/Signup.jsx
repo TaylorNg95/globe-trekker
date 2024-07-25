8ddbc258abc7445699e244f563bdee05304f1c69
Taylor Ng <taylormng95@gmail.com>
2024-07-25 12:18:33 -0400

    creates signup form and syncs to backend

--- client/src/components/sessions/Signup.jsx
+++ client/src/components/sessions/Signup.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import {useState} from 'react'
 
 function Signup() {
+
+  const initialFormData = {
+    name: '',
+    username: '',
+    password: ''
+  }
+  
+  const [formData, setFormData] = useState(initialFormData)
+
+  function handleChange(e){
+    const new_key = e.target.name
+    const new_value = e.target.value
+    setFormData({
+        ...formData, [new_key]: new_value
+    })
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    fetch('/api/signup', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+    })
+    setFormData(initialFormData)
+  }
+
   return (
-    <div>Signup</div>
+    <>
+        <h1>Sign Up Form</h1>
+        <form onSubmit={handleSubmit}>
+            <label>Name: <input type='text' name='name' value={formData.name} onChange={handleChange}/></label><br />
+            <label>Username: <input type='text' name='username' value={formData.username} onChange={handleChange}/></label><br />
+            <label>Password: <input type='password' name='password' value={formData.password} onChange={handleChange}/></label><br />
+            <input type='submit' value='Submit'/>
+        </form>
+    </>
   )
 }
 
