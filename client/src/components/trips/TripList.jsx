11d3ee8beb71cc7991e7ab3dd4d3fc08c49f1c01
Taylor Ng <taylormng95@gmail.com>
2024-07-25 17:07:07 -0400

    ensures UserTripsPage onld renders when user.trips has loaded

--- client/src/components/trips/TripList.jsx
+++ client/src/components/trips/TripList.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import TripCard from './TripCard'
 
-function TripList() {
+function TripList({trips}) {
+  const trips_list = trips
+  const tripCards = trips_list.map(trip => <TripCard key={trip.id} trip={trip}/>) 
+
   return (
     <div>TripList</div>
   )
--- client/src/components/trips/UserTripsPage.jsx
+++ client/src/components/trips/UserTripsPage.jsx
@@ -7,14 +7,16 @@ function UserTripsPage() {
 
   const {user} = useContext(UserContext)
 
-  return (
-    <>
-      <h1>User Trips Page</h1>
-      <h2>Here are your ongoing trips!</h2>
-      <TripList trips={user.trips}/>
-      <Link to='/trip-menu'>Start New Trip</Link>
-    </>
-  )
+  if (user.trips){
+    return (
+      <>
+        <h1>User Trips Page</h1>
+        <h2>Here are your ongoing trips!</h2>
+        <TripList trips={user.trips}/>
+        <Link to='/trip-menu'>Start New Trip</Link>
+      </>
+    )
+  }
 }
 
 export default UserTripsPage
