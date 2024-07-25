8f05a355a4de91e0e255331380d95e5d856523fd
Taylor Ng <taylormng95@gmail.com>
2024-07-25 12:26:38 -0400

    adds user and loggedIn states to UserContext

--- client/src/context/UserContext.jsx
+++ client/src/context/UserContext.jsx
@@ -3,9 +3,20 @@ import {createContext, useState} from 'react'
 const UserContext = createContext({})
 
 function UserProvider({children}) {
-    const [loggedIn, setLoggedIn] = useState(true)
+    const [loggedIn, setLoggedIn] = useState(null)
+    const [user, setUser] = useState(false)
 
-    return <UserContext.Provider value={{loggedIn}}>{children}</UserContext.Provider>
+    function login(user){
+        setLoggedIn = true
+        setUser(user)
+    }
+
+    function logout(){
+        setLoggedIn = false
+        setUser(null)
+    }
+
+    return <UserContext.Provider value={{loggedIn, user, login, logout}}>{children}</UserContext.Provider>
 }
 
 export {UserContext, UserProvider}
