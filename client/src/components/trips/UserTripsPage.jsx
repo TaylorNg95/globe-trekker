9d0f95c9c32772eaf32825471ec842a8335f64a2
Taylor Ng <taylormng95@gmail.com>
2024-07-26 08:57:28 -0400

    corrects userTripsPage to grab trips from user

--- client/src/components/trips/UserTripsPage.jsx
+++ client/src/components/trips/UserTripsPage.jsx
@@ -1,13 +1,14 @@
 import {useContext} from 'react'
 import { Link } from 'react-router-dom'
 import { UserContext } from '../../context/UserContext'
+import { TripContext } from '../../context/TripContext'
 import TripList from './TripList'
 
 function UserTripsPage() {
 
   const {user} = useContext(UserContext)
 
-  if (user.trips){
+  if (user){
     return (
       <>
         <h1>User Trips Page</h1>
