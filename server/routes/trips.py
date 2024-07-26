04d17bf260969331e0c9b0103405c2d13d771fc9
Taylor Ng <taylormng95@gmail.com>
2024-07-26 08:31:13 -0400

    removes User-specific Trips resource

--- server/routes/trips.py
+++ server/routes/trips.py
@@ -4,27 +4,24 @@ from flask_restful import Resource
 from models.user import User
 from models.trip import Trip
 
-class AllTripsResource(Resource):
+class TripsResource(Resource):
     def get(self):
         trips = Trip.query.all()
-        return [trip.to_dict(rules=['-entries',]) for trip in trips], 200
+        return [trip.to_dict() for trip in trips], 200
 
-class UserTripsResource(Resource):
+""" class UserTripsResource(Resource):
     def get(self, user_id):
         trips = {}
         for trip in User.query.filter(User.id == user_id).first().trips: # Avoids duplicate trips if multiple entries
             if not trips.get(trip):
                 trips[trip] = True
-        return [trip.to_dict(rules=['-entries',]) for trip in trips], 200
-        
-    # STRETCH GOAL: ADD CUSTOM TRIP
+        return [trip.to_dict(rules=['-entries',]) for trip in trips], 200 """
     
-class UserTripResource(Resource):
+""" class TripResource(Resource):
      def get(self, user_id, trip_id):
         trips = User.query.filter(User.id == user_id).first().trips
         trip = [trip for trip in trips if trip.id == trip_id][0]
-        return trip.to_dict(rules=['-entries',]), 200
+        return trip.to_dict(rules=['-entries',]), 200 """
 
-api.add_resource(AllTripsResource, '/api/trips')
-api.add_resource(UserTripsResource, '/api/users/<int:user_id>/trips')
-api.add_resource(UserTripResource, '/api/users/<int:user_id>/trips/<int:trip_id>')
+api.add_resource(TripsResource, '/api/trips')
+""" api.add_resource(TripResource, '/api/trips/<int:id>') """
