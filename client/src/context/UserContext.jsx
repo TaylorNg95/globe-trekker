1e8a532122405e980ead09b1f9b18ab248cf60c4
Taylor Ng <taylormng95@gmail.com>
2024-07-27 08:28:08 -0400

    creates async functions for UserContext

--- client/src/context/UserContext.jsx
+++ client/src/context/UserContext.jsx
@@ -29,8 +29,8 @@ function UserProvider({children}) {
         setUser(null)
     }
 
-    function addEntry(entry) {
-        fetch(`/api/entries`, {
+    async function addEntry(entry) {
+        const response = await fetch(`/api/entries`, {
             method: 'POST',
             headers: {
                 'Accept': 'application/json',
@@ -38,27 +38,21 @@ function UserProvider({children}) {
             },
             body: JSON.stringify(entry)
         })
-        .then(response => {
-            if (response.status == 201) {
-                return response.json()
-            } else if (response.status == 422) {
-                return response.json().then(error => {
-                    return Promise.reject(error)
-                })
-            }
-        })
-        .then(newEntry => {
-          setEntries([...entries, newEntry])
-        })
+        if (response.status == 201){
+            const newEntry = await response.json()
+            setEntries([...entries, newEntry])
+        } else {
+            console.log('Oops something went wrong')
+        }
     }
 
-    function deleteEntry(id) {
-        fetch(`/api/entries/${id}`, {
+    async function deleteEntry(id) {
+        const response = await fetch(`/api/entries/${id}`, {
           method: 'DELETE'
         })
-        .then(response => {
-          setEntries(entries.filter(ent => ent.id != id))
-        })
+        if (response.status == 204){
+            setEntries(entries.filter(ent => ent.id != id))
+        }
       }
 
     return <UserContext.Provider value={{loggedIn, user, login, logout, entries, setEntries, addEntry, deleteEntry}}>{children}</UserContext.Provider>
