ea6887f75ca0ca337a52c6850dbb0d6e24597718
Taylor Ng <taylormng95@gmail.com>
2024-07-27 19:20:26 -0400

    adds yup and formik to login

--- client/src/components/sessions/Login.jsx
+++ client/src/components/sessions/Login.jsx
@@ -1,37 +1,35 @@
-import {useState, useContext} from 'react'
+import {useContext} from 'react'
 import { UserContext } from '../../context/UserContext'
 import { useNavigate } from 'react-router-dom'
+import { useFormik } from 'formik'
+import * as yup from 'yup'
 
 function Login() {
 
   const {login} = useContext(UserContext)
   const navigate = useNavigate()
 
-  const initialFormData = {
+  const initialValues = {
     username: '',
     password: ''
   }
-  
-  const [formData, setFormData] = useState(initialFormData)
 
-  function handleChange(e){
-    const new_key = e.target.name
-    const new_value = e.target.value
-    setFormData({
-        ...formData, [new_key]: new_value
-    })
-  }
+  const validationSchema = yup.object({
+    username: yup.string().required(),
+    password: yup.string().required()
+  })
 
-  function handleSubmit(e) {
-    e.preventDefault()
-    async function loginUser() {
+  const formik = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit: async function(values) {
       const response = await fetch('/api/login', {
         method: 'POST',
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(formData)
+        body: JSON.stringify(values)
       })
       if (response.status == 200){
         const user = await response.json()
@@ -41,17 +39,15 @@ function Login() {
         const error = await response.json()
         console.log(error)
       }
-      setFormData(initialFormData)
     }
-    loginUser()
-  }
+  })
 
   return (
     <>
         <h1>Login Form</h1>
-        <form onSubmit={handleSubmit}>
-            <label>Username: <input type='text' name='username' value={formData.username} onChange={handleChange}/></label><br />
-            <label>Password: <input type='password' name='password' autoComplete='on' value={formData.password} onChange={handleChange}/></label><br />
+        <form onSubmit={formik.handleSubmit}>
+            <label>Username: <input type='text' name='username' value={formik.values.username} onChange={formik.handleChange}/></label><br />
+            <label>Password: <input type='password' name='password' autoComplete='on' value={formik.values.password} onChange={formik.handleChange}/></label><br />
             <input type='submit' value='Submit'/>
         </form>
     </>
