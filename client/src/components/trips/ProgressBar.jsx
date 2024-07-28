7d4055fca1b023b6ceb5b4a01f11524b2cb73a31
Taylor Ng <taylormng95@gmail.com>
2024-07-28 14:44:59 -0400

    corrects issue of users being able to view trips that arent theirs at the my-trips/:id route

--- client/src/components/trips/ProgressBar.jsx
+++ client/src/components/trips/ProgressBar.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
 function ProgressBar({progress, total}) {
+  let prog = progress
+  if (prog > total){
+    prog = total
+  }
+  // Account for if user inputs and entry that puts them over 100%
+
   return (
-    <div>Progress: {progress} miles || {total} total || {(progress / total * 100).toFixed(1)}% complete</div>
+    <div>Progress: {progress} miles || {total} total || {(prog / total * 100).toFixed(1)}% complete</div>
   )
 }
 
--- client/src/components/trips/entries/EntriesPage.jsx
+++ client/src/components/trips/entries/EntriesPage.jsx
@@ -12,18 +12,25 @@ function EntriesPage() {
   const trip = trips.find(trip => trip.id == id)
   
   const {entries} = useContext(UserContext)
-  const tripEntries = entries.filter(entry => entry.trip_id == trip.id && entry.miles != 0)
+  const tripEntries = entries.filter(entry => entry.trip_id == trip.id)
+  // Checks of there are any trip entries for the route (since user can theoretically navigate to a
+  // 'my-trips/:id' route without it being theirs); if none, nothing will be rendered
+  const tripEntriesOverZero = tripEntries.filter(entry => entry.miles != 0)
+  // We don't want to show trip entries where miles = 0; these can arise in the particular case when a
+  // user has created a custom trip but not added any new entries
   const milesAchieved = tripEntries.reduce((accumulator, entry) => {
     return Math.round(accumulator + entry.miles) // TBD - not DRY with MyTripsCard
   }, 0)
 
-  return (
+  if (tripEntries.length == 0) {
+    return null
+  } else return (
     <>
       <h1>{trip.name}</h1>
       <h2>{trip.total_miles}</h2>
       <ProgressBar progress={milesAchieved} total={trip.total_miles}/>
       <div>
-        {tripEntries.map(entry => <EntryItem key={entry.id} entry={entry} trip={trip}/>)}
+        {tripEntriesOverZero.map(entry => <EntryItem key={entry.id} entry={entry} trip={trip}/>)}
         <EntryForm trip={trip} editMode={false}/>
       </div>
     </>
