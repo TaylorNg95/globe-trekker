c4f9f3b8460bf6a4e294efa2ebd5aab4b0a3973c
Taylor Ng <taylormng95@gmail.com>
2024-07-28 15:31:33 -0400

    adds Material UI theme and CSS baseline;

---
+++ client/src/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#89CFF0',
+    },
+    secondary: {
+      main: '#FC6A03',
+    }
+  },
+});
+
+export default theme;
