7d9d218be4288bb8b10d4d5ed35cd1856559a1d8
Taylor Ng <taylormng95@gmail.com>
2024-07-29 09:39:26 -0400

    styles home page

--- client/src/components/static/Home.jsx
+++ client/src/components/static/Home.jsx
@@ -1,10 +1,17 @@
-import { Container, Typography } from '@mui/material'
-import React from 'react'
+import {useContext} from 'react'
+import { UserContext } from '../../context/UserContext'
+import {Link} from 'react-router-dom'
+
+// MATERIAL UI
+import { Container, Typography, Button } from '@mui/material'
 
 function Home() {
-    return (   
-        <Container sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '60vh'}}>
-            <Typography component='h1' variant='h3'>Walking the World</Typography>
+    const {user} = useContext(UserContext)
+
+    return (
+        <Container sx={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', minHeight: '80vh'}}>
+            <Typography component='h1' variant='h3'>{user ? `Welcome, ${user.name}!` : 'Walking the World'}</Typography>
+            {user ? <Button component={Link} to='/my-trips' variant='contained' sx={{mt: 2}}>View My Trips</Button> : null}
         </Container>
     )
 }
