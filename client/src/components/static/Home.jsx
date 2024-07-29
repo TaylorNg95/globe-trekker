bfd9160f3a1c43670828e20418460460ee01463f
Taylor Ng <taylormng95@gmail.com>
2024-07-29 15:50:04 -0400

    adds shoe and button to home component

--- client/src/components/static/Home.jsx
+++ client/src/components/static/Home.jsx
@@ -1,6 +1,7 @@
 import {useContext} from 'react'
 import { UserContext } from '../../context/UserContext'
 import {Link} from 'react-router-dom'
+import './Home.css'
 
 // MATERIAL UI
 import { Container, Typography, Button } from '@mui/material'
@@ -9,9 +10,12 @@ function Home() {
     const {user} = useContext(UserContext)
 
     return (
-        <Container sx={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', minHeight: '80vh'}}>
+        <Container id='welcome' sx={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', minHeight: '80vh'}}>
             <Typography component='h1' variant='h3'>{user ? `Welcome, ${user.name}!` : 'Walking the World'}</Typography>
-            {user ? <Button component={Link} to='/my-trips' variant='contained' sx={{mt: 2}}>View My Trips</Button> : null}
+            <img id='boots' src='/images/boots.png'></img>
+            {user ?
+                <Button component={Link} to='/my-trips' variant='contained' sx={{mt: 3}}>View My Trips</Button> :
+                <Button component={Link} to='/login' variant='contained' sx={{mt: 3}}>Login</Button>}
         </Container>
     )
 }
